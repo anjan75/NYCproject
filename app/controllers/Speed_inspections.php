1a956f3b5b171b22fc51799facfa068b35ecb425
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 16:00:15 -0400

    Upload New File

---
+++ app/controllers/Speed_inspections.php
@@ -0,0 +1,23 @@
+<?php
+
+class Speed_inspections extends Controller {
+
+  public function __construct() {
+    if(!isLoggedIn()) {
+      redirect('users/login');
+
+    //$this->postModel = $this->model('Post');
+    //$this->userModel = $this->model('User');
+    }
+  }
+  
+
+  public function index() {
+    //get posts
+    //$id = $_SESSION['user_id'];
+
+    //$posts = $this->postModel->getPostsByUserId($id);
+
+    $this->view('speed_inspections/index');
+  }
+}
