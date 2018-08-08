e57573c1d1fb76a4b2d230fa00087c606b560808
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 16:23:05 -0400

    Upload New File

---
+++ app/views/inspection_search/index.php
@@ -0,0 +1,135 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h3>Inspection Search</h2>
+<hr />
+<form  action="">
+  <div class="row">
+  <div class="col-md-2">
+	<div class="form-group">
+	<label for="" style="font-size: 90%;">Inspection ID</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-2">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Observation ID</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-1">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Start Date</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-1">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">End Date</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-2">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Type of Inspection</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-2">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Result</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-2">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Comments-Key Words</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  </div>
+  <div class="row">
+  <div class="col-md-2">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Observation Employee</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-2">
+  <div class="form-group">
+  <label for="" style="font-size: 90%;">Testing Officer</label>
+  <input type="" class="form-control" id="" name="">
+  </div>
+  </div>
+  <div class="col-md-6">
+  </div>
+  <div class="col-md-1">
+  <div class="form-group">
+  <label class="invisible" for="">Filter</label>
+  <button id="" name="" class="btn btn-primary">Filter</button>
+  </div>
+  </div>
+  <div class="col-md-1">
+  <div class="form-group">
+  <label class="invisible" for="">Clear</label>
+  <button id="" name="" class="btn btn-primary">Clear</button>
+  </div>
+  </div>
+  </div>
+  <hr />
+
+  <table class="table table-bordered table-striped table-sm">
+    <thead class="thead-dark" style="background-color: black; color: white; font-size: 70%">
+    <tr>
+      <th scope="col" style="width: 3%">Modify</th>
+      <th scope="col" style="width: 3%">Delete</th>
+      <th scope="col" style="width: 5%; text-align: center;">Inspection ID</th>
+      <th scope="col" style="width: 5%; text-align: center;">Observation ID</th>
+      <th scope="col" style="width: 5%; text-align: center;">Type of Inspection</th>
+      <th scope="col" style="width: 5%; text-align: center;">Testing Officer</th>
+      <th scope="col" style="width: 5%; text-align: center;">Observation Employee</th>
+      <th scope="col" style="width: 5%; text-align: center;">Depatrment</th>
+      <th scope="col" style="width: 5%; text-align: center;">Occupation</th>
+      <th scope="col" style="text-align: center;">Task</th>
+      <th scope="col" style="width: 5%; text-align: center;">Location Type</th>
+      <th scope="col" style="width: 5%; text-align: center;">Location</th>
+      <th scope="col" style="width: 5%; text-align: center;">Result</th>
+      <th scope="col" style="width: 15%; text-align: center;">Comments</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    </tbody>
+  </table>
+
+</form>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
