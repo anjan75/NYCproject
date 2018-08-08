f621e083f9f8aa2a41ba2eeb94023a8cfe6d439a
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 15:50:08 -0400

    Upload New File

---
+++ app/controllers/Entry_restrictions.php
@@ -0,0 +1,23 @@
+<?php
+
+class Entry_restrictions extends Controller {
+
+  public function __construct() {
+    if(!isLoggedIn()) {
+      redirect('users/login');
+
+    //$this->postModel = $this->model('Post');
+    //$this->userModel = $this->model('User');
+    }
+  }
+  
+
+  public function index() {
+    //get posts
+    //$id = $_SESSION['user_id'];
+
+    //$posts = $this->postModel->getPostsByUserId($id);
+
+    $this->view('entry_restrictions/index');
+  }
+}
