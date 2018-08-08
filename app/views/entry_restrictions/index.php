313b01bb74c2f0aecb130e03a78da14ac4cc5e26
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 16:21:29 -0400

    Upload New File

---
+++ app/views/entry_restrictions/index.php
@@ -0,0 +1,24 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h3>Inspection/Observation Entry Restrictions Administration</h2>
+<hr />
+<form  action="">
+	<div class="form-group form-inline">
+	<label class="col-md-4" for="" style="margin-right: -20px;">Observation Date Entry Restriction</label>
+    <input type="" class="col-md-1 form-control" id="" name="" style="margin-right: 10px;">
+     <input type="" class="col-md-1 form-control" id="" name="">
+    </div>
+
+
+    <div class="text-center"> 
+     <div class="row">
+     	<div class="col-md-3"> 
+        <input type="button" class="btn btn-primary" style="margin-top: 10px; margin-right: -400px;" value="Save">
+     	</div>
+     	<div class="col-md-3">
+     		<button id="" name="" class="btn btn-primary" style="margin-top: 10px;">Reset</button> 
+     	</div>
+     </div>
+    </div>
+
+</form>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
