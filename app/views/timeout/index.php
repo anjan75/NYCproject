5dbd44be3a3ce58af398e3c2536f27fea2540e76
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 16:27:56 -0400

    Upload New File

---
+++ app/views/timeout/index.php
@@ -0,0 +1,15 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h3>System Idle Timeout Administration</h3>
+<hr />
+<form  action="">
+  <div class="row">
+    <div class="form-group form-inline">
+    <label class="col-md-4" for="" style="margin-right: -20px;">Timeout Time</label>
+    <input type="" class="col-md-5 form-control" id="" name="">
+  <button id="" name="" class="ml-auto btn btn-primary">Save</button>
+  <button id="" name="" class="ml-auto btn btn-primary">Reset</button>
+  </div>
+
+
+</form>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
