9e2e87305bcd2819d196aba02dfda0c75cceabd3
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 16:22:18 -0400

    Upload New File

---
+++ app/views/inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+  <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+  <?php require APPROOT . '/views/inc/navbar.php'; ?>
+  <div class="container">
+  
