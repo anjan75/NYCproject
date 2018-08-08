682e9311b017c8bc3ba65d63c26ffc046ef4b6c0
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 15:51:42 -0400

    Upload New File

---
+++ app/controllers/Locations.php
@@ -0,0 +1,23 @@
+<?php
+
+class Locations extends Controller {
+
+  public function __construct() {
+    if(!isLoggedIn()) {
+      redirect('users/login');
+
+    //$this->postModel = $this->model('Post');
+    //$this->userModel = $this->model('User');
+    }
+  }
+  
+
+  public function index() {
+    //get posts
+    //$id = $_SESSION['user_id'];
+
+    //$posts = $this->postModel->getPostsByUserId($id);
+
+    $this->view('locations/index');
+  }
+}
