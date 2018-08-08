9cbd527992a8697babe22355c0494f3d0a96f027
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 15:44:49 -0400

    Upload New File

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '091811030');
+  define('DB_NAME', 'tmvc');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/ecr2');
+  // Site Name
+  define('SITENAME', 'ECR2');
+  // App Version
+  define('APPVERSION', '1.0.0');
