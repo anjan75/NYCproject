622aa9f6084a15649003d6f7c012e07bc6593385
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 15:49:06 -0400

    Upload New File

---
+++ app/controllers/CarMover_inspections.php
@@ -0,0 +1,23 @@
+<?php
+
+class CarMover_inspections extends Controller {
+
+  public function __construct() {
+    if(!isLoggedIn()) {
+      redirect('users/login');
+
+    //$this->postModel = $this->model('Post');
+    //$this->userModel = $this->model('User');
+    }
+  }
+  
+
+  public function index() {
+    //get posts
+    //$id = $_SESSION['user_id'];
+
+    //$posts = $this->postModel->getPostsByUserId($id);
+
+    $this->view('carMover_inspections/index');
+  }
+}
