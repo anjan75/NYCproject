9c338b2ecd4d0990f6ad1441029fa50bf36eb093
Ruoxi Geng <gengruoxi2013@gmail.com>
2018-08-08 15:49:51 -0400

    Upload New File

---
+++ app/controllers/Email_groups.php
@@ -0,0 +1,23 @@
+<?php
+
+class Email_groups extends Controller {
+
+  public function __construct() {
+    if(!isLoggedIn()) {
+      redirect('users/login');
+
+    //$this->postModel = $this->model('Post');
+    //$this->userModel = $this->model('User');
+    }
+  }
+  
+
+  public function index() {
+    //get posts
+    //$id = $_SESSION['user_id'];
+
+    //$posts = $this->postModel->getPostsByUserId($id);
+
+    $this->view('email_groups/index');
+  }
+}
